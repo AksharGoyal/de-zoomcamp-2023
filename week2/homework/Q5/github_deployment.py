5b9ce65fc6f0110d655262f410bd05a05ebec7c6
Akshar Goyal <claim.akshar@gmail.com>
2023-02-08 03:02:15 +0530

    Update github_deployment.py

--- week2/homework/Q5/github_deployment.py
+++ week2/homework/Q5/github_deployment.py
@@ -2,13 +2,13 @@ from prefect.deployments import Deployment
 from prefect.filesystems import GitHub
 from web_to_gcs_homework_q5 import etl_gcs_to_bq
 
-github_block = GitHub.load("prefect-zoomcamp-github")
+github_block = GitHub.load("blockname")
 
 github_deployment = Deployment.build_from_flow(
     flow=etl_gcs_to_bq,
     name='Load to GCS via Github',
     storage=github_block,
-    entrypoint='week2/homework/Q5/web_to_gcs_bq.py:etl_gcs_to_bq',
+    entrypoint='entrypoint:flow',
     parameters={'months':[4], 'color':'green', 'year':2019}
 )
 
